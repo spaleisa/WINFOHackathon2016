a7914e9cd3aa1c815fdcc35068bb9a36ba725ed8
Isabella Spaletta <spaleisa@uw.edu>
2016-01-30 15:17:56 -0800

    Fixing problem of having two js files

--- app.js
+++ app.js
@@ -240,6 +240,8 @@ function success(pos) {
     console.log('Longitude: ' + crd.longitude);
     console.log('More or less ' + crd.accuracy + ' meters.');
 
+    console.log(distance(crd.latitude, crd.longitude, 180, 180))
+
 };
 
 navigator.geolocation.getCurrentPosition(success);
@@ -254,4 +256,28 @@ navigator.geolocation.getCurrentPosition(success);
 //     console.log(12742 * Math.asin(Math.sqrt(a));
 //     return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
 // }
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+    var radlat1 = Math.PI * lat1/180
+    var radlat2 = Math.PI * lat2/180
+    var theta = lon1-lon2
+    var radtheta = Math.PI * theta/180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist)
+    dist = dist * 180/Math.PI
+    dist = dist * 60 * 1.1515
+    if (unit=="K") { dist = dist * 1.609344 }
+    if (unit=="N") { dist = dist * 0.8684 }
+    return dist
+}
+
+
+
+
+
+
+
+
+
+
 
