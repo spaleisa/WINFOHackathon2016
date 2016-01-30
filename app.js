c0aeff257073f6d85a09ea7dc4e6c8d92e7cd0eb
Isabella Spaletta <spaleisa@uw.edu>
2016-01-30 15:08:24 -0800

    updating to get rid of error

--- app.js
+++ app.js
@@ -223,12 +223,28 @@ function logInSignUp(email, password, $scope, $firebaseObject, $firebaseAuth, $l
 }
 
 function success(pos) {
-  var crd = pos.coords;
+    var crd = pos.coords;
 
-  console.log('Your current position is:');
-  console.log('Latitude : ' + crd.latitude);
-  console.log('Longitude: ' + crd.longitude);
-  console.log('More or less ' + crd.accuracy + ' meters.');
+    var lat1 = crd.latitude;
+    var lon1 = crd.longitude;
+
+    console.log('Your current position is:');
+    console.log('Latitude : ' + crd.latitude);
+    console.log('Longitude: ' + crd.longitude);
+    console.log('More or less ' + crd.accuracy + ' meters.');
+
 };
 
 navigator.geolocation.getCurrentPosition(success);
+
+// function distance(lat1, lon1, lat2, lon2) {
+//     var p = 0.017453292519943295;    // Math.PI / 180
+//     var c = Math.cos;
+//     var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+//           c(lat1 * p) * c(lat2 * p) * 
+//           (1 - c((lon2 - lon1) * p))/2;
+
+//     console.log(12742 * Math.asin(Math.sqrt(a));
+//     return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+// }
+
