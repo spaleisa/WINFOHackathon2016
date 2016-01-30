8a25f68e793633cf2c9fd0e0c71a1f6b592b7cd6
Katrina Ezis <katrinaezis@gmail.com>
2016-01-30 12:24:57 -0800

    init

--- app.js
+++ app.js
@@ -1,4 +1,89 @@
 /**
  * Created by Brad on 1/30/2016.
  */
-var myApp = angular.module('myApp', ['firebase']);
+var myApp = angular.module('myApp', ['firebase', 'ui.router']);
+var ref = new Firebase("https://winfo.firebaseio.com/");
+
+myApp.config(function($stateProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'templates/home.html',
+      controller: 'HomeController'
+    })
+});
+
+myApp.controller('HomeController', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject, $http, $location) {
+	
+});
+
+function logInSignUp(name, email, password, $scope, $firebaseObject, $firebaseAuth, $location, $http){
+	// Create a variable 'ref' to reference your firebase storage
+    var userRef = ref.child("users");
+
+    // Create a firebaseObject of your users, and store this as part of $scope
+    $scope.users = $firebaseObject(userRef);
+	
+    // Create authorization object that referes to firebase
+    $scope.authObj = $firebaseAuth(ref);
+	
+    // Test if already logged in
+    var authData = $scope.authObj.$getAuth();
+    if (authData) {
+        $scope.userId = authData.uid;
+    }
+    // SignUp function
+	$scope.signUp = function() {
+        // Create user
+		// Here, you set default values for users if there is any
+		$scope.create = false;
+
+		// Authenticate user
+        $scope.authObj.$createUser({
+            email: $scope.email,
+            password: $scope.password
+        })
+
+        // Once the user is created, call the logIn function
+        .then($scope.logIn)
+
+        // Catch any errors
+        .catch(function(error) {
+            console.error("Error: ", error);
+			console.log(error.code);
+			if(error.code == "INVALID_EMAIL"){
+				alert("Email is invalid")
+			}
+			if(error.code == "EMAIL_TAKEN"){
+				alert("Email already in use")
+			}
+        });
+    }
+
+    // SignIn function, reads whatever set-up the user has
+    $scope.signIn = function() {
+        $scope.logIn().then(function(authData){
+            $scope.userId = authData.uid
+			console.log($scope.userId)
+			var id = $scope.userId;
+			$scope.badges = $scope.users[id].badges
+			location.reload();
+        })
+    }
+	
+    // LogIn function
+    $scope.logIn = function() {
+        return $scope.authObj.$authWithPassword({
+            email: $scope.email,
+            password: $scope.password
+        })
+    }
+	
+    // LogOut function
+    $scope.logOut = function() {
+        $scope.authObj.$unauth()
+        $scope.userId = false
+		$location.path('/')
+		$scope.badges = []
+    }
+}
